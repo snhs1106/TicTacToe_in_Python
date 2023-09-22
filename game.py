35c2fb93f76596ef20490cbe70203ee7b5f18849
snhs1106 <stephanieh0611@gmail.com>
2023-09-21 21:28:55 -0400

    Added a function to assign X or O to the players

--- game.py
+++ game.py
@@ -7,3 +7,16 @@ def display_board(board):
     print(board[7] + '|' + board[8] + '|' + board[9])
     print(board[4] + '|' + board[5] + '|' + board[6])
     print(board[1] + '|' + board[2] + '|' + board[3])
+
+def player_input():
+
+    marker = 'wrong'
+
+    while marker != 'X' and marker != 'O':
+        marker = int(input('Player 1 choose either X or O: ')).upper()
+    
+    if marker == 'X':
+        return('X','O')
+    else:
+        return('O','X')
+
