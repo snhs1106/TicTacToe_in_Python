eeb19f703cbdbdc2d75469d0f565df449ec80e45
snhs1106 <stephanieh0611@gmail.com>
2023-09-22 19:29:28 -0400

    Created function to check if there is a winner

--- game.py
+++ game.py
@@ -27,3 +27,14 @@ def player_marker(board, marker, position):
     #Assign either X or O to a specific position on the board
     board[position] = marker
 
+def win_check(board, marker):
+
+    return((board[1] == board[2] == board[3] == marker) or 
+    (board[4] == board[5] == board[6] == marker) or 
+    (board[7] == board[8] == board[9] == marker) or 
+    (board[1] == board[4] == board[7] == marker) or
+    (board[2] == board[5] == board[8] == marker) or
+    (board[3] == board[6] == board[9] == marker) or 
+    (board[1] == board[5] == board[9] == marker) or
+    (board[3] == board[5] == board[7] == marker))
+
