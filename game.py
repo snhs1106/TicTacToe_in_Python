566e4ea2759bd321ad531b09b68fb264edcd3ad2
snhs1106 <stephanieh0611@gmail.com>
2023-09-22 22:09:42 -0400

    Allows a player to pick a spot on the board

--- game.py
+++ game.py
@@ -63,3 +63,12 @@ def full_board(board):
         if space_check(board, i):
             return False
     return True
+
+def player_choice(board):
+    position = 0
+    
+    while position not in range(1,10) or not space_check(board, position):
+        position = int(input('Choose a position: (1-9)'))
+    
+    return position
+
